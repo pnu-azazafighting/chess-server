4d12d262d618ff87648eea3acd5567d420d47a01
Jieon Kim <eoniedev@gmail.com>
2023-03-25 01:10:35 +0900

    chore: 프로젝트 초기 설정 (#1)

---
+++ src/test/java/com/example/chessserver/ChessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chessserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
