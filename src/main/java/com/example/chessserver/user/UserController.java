5f5d6580ebdb48e9a9ba4ed5c78f01404cf71f69
jieonkim23 <kwldjs1023@gmail.com>
2023-03-31 00:57:13 +0900

    fix: user uuid를 리턴하도록 /users 컨트롤러 수정, 대기리스트에 추가

--- src/main/java/com/example/chessserver/user/UserController.java
+++ src/main/java/com/example/chessserver/user/UserController.java
@@ -1,5 +1,6 @@
 package com.example.chessserver.user;
 
+import com.example.chessserver.multi.service.MatchService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,14 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final MatchService matchService;
+
     @Operation(summary = "닉네임 입력", description = "닉네임 입력시 해당 유저의 고유 아이디 리턴하는 api")
     @PostMapping("/users")
     public ResponseEntity<UserDto> responseUserId(@RequestBody String nickName) {
-        String id = userService.generateUserId();
+        String uuid = userService.saveUser(nickName);
+        matchService.insertWaitingList(uuid);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(UserDto.builder()
-                    .userId(id).build());
+                        .uuid(uuid).build());
     }
 }
