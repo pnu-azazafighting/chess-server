28dbedd3d352cf17a191cd2f1615ab20d10b5b8e
jieonkim23 <kwldjs1023@gmail.com>
2023-03-31 00:54:52 +0900

    feat: user service 생성 및 유저 저장

--- src/main/java/com/example/chessserver/user/UserService.java
+++ src/main/java/com/example/chessserver/user/UserService.java
@@ -1,12 +1,23 @@
 package com.example.chessserver.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.UUID;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class UserService {
-    public String generateUserId() {
+    private final UserRepository userRepository;
+    public String saveUser(String name) {
+        User user = new User(name, generateUUId());
+        userRepository.save(user);
+        return user.getUuid();
+    }
+
+    public String generateUUId() {
         return UUID.randomUUID().toString();
     }
 }
