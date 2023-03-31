857568c86fd18213afe6344ad952190fd2fffab6
jieonkim23 <kwldjs1023@gmail.com>
2023-03-31 15:06:36 +0900

    feat: match를 스케쥴러로 변경, uuid로 게임찾는 메소드 구현

--- src/main/java/com/example/chessserver/multi/service/MatchService.java
+++ src/main/java/com/example/chessserver/multi/service/MatchService.java
@@ -2,39 +2,61 @@ package com.example.chessserver.multi.service;
 
 import com.example.chessserver.multi.domain.Game;
 import com.example.chessserver.multi.data.GameDto;
-import com.example.chessserver.multi.domain.Player;
+import com.example.chessserver.multi.repository.GameRepository;
+import com.example.chessserver.user.User;
+import com.example.chessserver.user.UserRepository;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.*;
 
+@EnableScheduling
+@Transactional
 @Service
 public class MatchService {
-    private final Map<String, Game> games = new HashMap<>();
-    private final Queue<Player> playerList = new LinkedList<>();
+    private volatile Map<String, Game> games;
+    private volatile Queue<String> playerList;
+    private final GameRepository gameRepository;
+    private final UserRepository userRepository;
 
-    public GameDto match(String playerId) {
-        if (games.containsKey(playerId)) {
+    public MatchService(GameRepository gameRepository, UserRepository userRepository) {
+        this.gameRepository = gameRepository;
+        this.userRepository = userRepository;
+        games = new HashMap<>();
+        playerList = new LinkedList<>();
+    }
+
+    public GameDto getGameUuid(String playerUuid) {
+        if (games.containsKey(playerUuid)) {
             return GameDto.builder()
-                    .id(games.get(playerId).getId())
+                    .uuid(games.get(playerUuid).getUuid())
                     .build();
         }
-        synchronized (playerList) {
-            if (!containPlayer(playerId)) {
-                playerList.add(new Player(playerId));
-            }
-            while (playerList.size() >= 2) {
-                Player player1 = playerList.poll();
-                Player player2 = playerList.poll();
-                Game game = new Game(UUID.randomUUID().toString(), player1, player2);
-                games.put(player1.getId(), game);
-                games.put(player2.getId(), game);
-            }
-        }
-
         return GameDto.builder().build();
     }
 
-    private boolean containPlayer(String playerId) {
-        return playerList.stream().anyMatch(player -> player.getId().equals(playerId));
+    public void insertWaitingList(String uuid) {
+        if (!games.containsKey(uuid) && !playerList.contains(uuid))
+            playerList.add(uuid);
+    }
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 3000)
+    public void matchGame() {
+        synchronized (playerList) {
+            while (playerList.size() >= 2) {
+                String uuid1 = playerList.poll();
+                String uuid2 = playerList.poll();
+                User player1 = userRepository.findByUuid(uuid1);
+                User player2 = userRepository.findByUuid(uuid2);
+
+                Game game = new Game(UUID.randomUUID().toString(), player1, player2);
+                gameRepository.saveGame(game);
+
+                games.put(player1.getUuid(), game);
+                games.put(player2.getUuid(), game);
+            }
+        }
     }
 }
