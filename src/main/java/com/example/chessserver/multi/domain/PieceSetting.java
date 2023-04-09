d288a99ee1ff693d393c795f64735e0903d69031
jieonkim23 <kwldjs1023@gmail.com>
2023-04-09 16:52:24 +0900

    feat: PieceSetting 생성

---
+++ src/main/java/com/example/chessserver/multi/domain/PieceSetting.java
@@ -0,0 +1,30 @@
+package com.example.chessserver.multi.domain;
+
+import com.example.chessserver.multi.data.PieceDto;
+
+import java.util.List;
+
+public class PieceSetting {
+    private final List<Integer> king;
+    private final List<Integer> bishop;
+    private final List<Integer> rook;
+    private final List<Integer> pawn;
+    private final List<Integer> trap;
+
+    public PieceSetting(List<Integer> king, List<Integer> bishop, List<Integer> rook, List<Integer> pawn, List<Integer> trap) {
+        this.king = king;
+        this.bishop = bishop;
+        this.rook = rook;
+        this.pawn = pawn;
+        this.trap = trap;
+    }
+
+    public PieceDto toPieceDto() {
+        return PieceDto.builder()
+                .king(king)
+                .bishop(bishop)
+                .pawn(pawn)
+                .rook(rook)
+                .trap(trap).build();
+    }
+}
