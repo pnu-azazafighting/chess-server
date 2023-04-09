d2c3213e61fd9e938da73c7f82f0e974273d9633
jieonkim23 <kwldjs1023@gmail.com>
2023-04-09 17:25:14 +0900

    fix: /match api request body를 json으로 변경

--- src/main/java/com/example/chessserver/multi/controller/MatchController.java
+++ src/main/java/com/example/chessserver/multi/controller/MatchController.java
@@ -2,6 +2,7 @@ package com.example.chessserver.multi.controller;
 
 import com.example.chessserver.multi.data.GameDto;
 import com.example.chessserver.multi.service.MatchService;
+import com.example.chessserver.user.data.UserDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -20,13 +21,13 @@ import java.util.Objects;
 @RequestMapping("/matchs")
 public class MatchController {
     private final MatchService matchService;
-    @Operation(summary = "닉네임 입력", description = "닉네임 입력시 퀵 매칭 후 룸 번호를 리턴하는 api")
+    @Operation(summary = "유저 아이디 입력", description = "유저 아이디 입력시 퀵 매칭 후 게임 룸 아이디를 리턴하는 api")
     @ApiResponse(responseCode = "200", description = "게임 매칭 성공")
     @ApiResponse(responseCode = "202", description = "아직 매칭되지 않음")
     @PostMapping
-    public ResponseEntity<GameDto> quickMatch(@RequestBody String userUuid) {
-        GameDto gameDto = matchService.getGameUuid(userUuid);
-        if(Objects.isNull(gameDto.getUuid())) {
+    public ResponseEntity<GameDto> quickMatch(@RequestBody UserDto userDto) {
+        GameDto gameDto = matchService.getGameUuid(userDto.getUserId());
+        if(Objects.isNull(gameDto.getGameId())) {
             return ResponseEntity
                     .status(HttpStatus.ACCEPTED).build();
         }
