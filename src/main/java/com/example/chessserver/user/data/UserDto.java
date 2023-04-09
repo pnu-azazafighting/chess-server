992ff4368a52dd7e5d6282493167551083b7da3a
jieonkim23 <kwldjs1023@gmail.com>
2023-04-09 16:49:27 +0900

    fix: UserDto uuid를 유저아이디로 변경 및 기본생성자 추가

--- src/main/java/com/example/chessserver/user/data/UserDto.java
+++ src/main/java/com/example/chessserver/user/data/UserDto.java
@@ -1,13 +1,15 @@
 package com.example.chessserver.user.data;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class UserDto {
-    private final String uuid;
-    @Builder
-    private UserDto(String uuid) {
-        this.uuid = uuid;
+    private String userId;
+    public UserDto() {
+
+    }
+
+    public UserDto(String userId) {
+        this.userId = userId;
     }
 }
