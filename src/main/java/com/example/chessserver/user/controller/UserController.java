24eb2a28216e8ce8706b6432fa366ad41bf378f2
jieonkim23 <kwldjs1023@gmail.com>
2023-05-31 14:32:13 +0900

    fix: CrossOrigin 모두 허용

--- src/main/java/com/example/chessserver/user/controller/UserController.java
+++ src/main/java/com/example/chessserver/user/controller/UserController.java
@@ -9,12 +9,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 public class UserController {
     private final UserService userService;
     private final MatchService matchService;
