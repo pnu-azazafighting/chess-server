21cf28e6c87a1b78754775431840bee62d8813df
jieonkim23 <kwldjs1023@gmail.com>
2023-05-31 23:20:21 +0900

    feat: findOtherPlayerId 메소드 추출 및 정보 없을 시 NoContentException 던지도록 수정

--- src/main/java/com/example/chessserver/multi/service/MultiGameService.java
+++ src/main/java/com/example/chessserver/multi/service/MultiGameService.java
@@ -8,6 +8,8 @@ import com.example.chessserver.multi.domain.Game;
 import com.example.chessserver.multi.domain.Piece;
 import com.example.chessserver.multi.domain.PieceMovement;
 import com.example.chessserver.multi.domain.PieceSetting;
+import com.example.chessserver.multi.exception.NoContentException;
+import com.example.chessserver.multi.exception.NoGameException;
 import com.example.chessserver.multi.repository.GameRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -25,49 +28,59 @@ public class MultiGameService {
     private final GameRepository gameRepository;
     private final MatchService matchService;
 
-    public void savePieceSetting(PieceReqDto pieceReqDto) {
+    public void savePieceSetting(String gameUuid, PieceReqDto pieceReqDto) {
         settings.put(pieceReqDto.getUserId(), pieceReqDto.getPiece().toPieceSetting());
+
+        String id = findOtherPlayerId(gameUuid, pieceReqDto.getUserId());
+        settings.remove(id);
     }
 
     public PieceResDto getPieceSetting(String gameUuid, String userUuid) {
-        Game game = gameRepository.findGameByUuid(gameUuid);
-        String targetUuid;
-        if(game.getPlayer1().getUuid().equals(userUuid)) {
-            targetUuid = game.getPlayer2().getUuid();
+        Game game = gameRepository.findGameByUuid(gameUuid).orElseThrow(NoGameException::new);
+        String targetUuid = findOtherPlayerId(gameUuid, userUuid);
+        int sequence = game.getPlayer1().getUuid().equals(userUuid) ? 1 : 2;
+        if(Objects.nonNull(settings.get(targetUuid))){
             return PieceResDto.builder()
                     .piece(settings.get(targetUuid).toPieceDto())
-                    .sequence(1).build();
+                    .sequence(sequence).build();
         }
-        targetUuid = game.getPlayer1().getUuid();
-        return PieceResDto.builder()
-                .piece(settings.get(targetUuid).toPieceDto())
-                .sequence(2).build();
+
+        throw new NoContentException();
     }
-    public void saveMovement(MoveReqDto moveReqDto) {
+    public void saveMovement(String gameUuid, MoveReqDto moveReqDto) {
         PieceMovement pieceMovement = new PieceMovement(
                 Piece.toPiece(moveReqDto.getPiece()),
                 moveReqDto.getStart(),
                 moveReqDto.getEnd());
         movements.put(moveReqDto.getUserId(), pieceMovement);
+
+        String id = findOtherPlayerId(gameUuid, moveReqDto.getUserId());
+        movements.remove(id);
     }
 
     public MoveResDto getMovement(String gameUuid, String userUuid) {
-        Game game = gameRepository.findGameByUuid(gameUuid);
-        PieceMovement pieceMovement;
-        if(game.getPlayer1().getUuid().equals(userUuid)) {
-            pieceMovement = movements.get(game.getPlayer2().getUuid());
+        PieceMovement pieceMovement = movements.get(findOtherPlayerId(gameUuid, userUuid));
+        if(Objects.nonNull(pieceMovement)) {
+            return MoveResDto.builder()
+                    .piece(pieceMovement.getPiece().toString())
+                    .start(pieceMovement.getStart())
+                    .end(pieceMovement.getEnd()).build();
+
         }
-        else{
-            pieceMovement = movements.get(game.getPlayer1().getUuid());
-        }
-        return MoveResDto.builder()
-                .piece(pieceMovement.getPiece().toString())
-                .start(pieceMovement.getStart())
-                .end(pieceMovement.getEnd()).build();
+        throw new NoContentException();
     }
 
     public void deleteGame(String gameUuid) {
         matchService.deleteGame(gameUuid);
         gameRepository.removeGame(gameUuid);
     }
+
+    private String findOtherPlayerId(String gameUuid, String userUuid) {
+        Game game = gameRepository.findGameByUuid(gameUuid).orElseThrow(NoGameException::new);
+        if(game.getPlayer1().getUuid().equals(userUuid)) {
+            return game.getPlayer2().getUuid();
+        }
+        return game.getPlayer1().getUuid();
+    }
 }
+
