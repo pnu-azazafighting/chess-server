21ed3bcd401ee75b482454c24d19c1d001216088
jieonkim23 <kwldjs1023@gmail.com>
2023-06-08 10:50:32 +0900

    feat: 숫자게임 세팅 응답 dto NumSetRes 생성

---
+++ src/main/java/com/example/chessserver/number/data/NumSetRes.java
@@ -0,0 +1,17 @@
+package com.example.chessserver.number.data;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Map;
+@Getter
+public class NumSetRes {
+    private final int sequence;
+    private final Map<Integer, List<Integer>> nums;
+    @Builder
+    private NumSetRes(int sequence, Map<Integer, List<Integer>> nums) {
+        this.sequence = sequence;
+        this.nums = nums;
+    }
+}
