717dbeb856749e8bcc2cea75078197c12b9cdfd2
Jieon Kim <eoniedev@gmail.com>
2023-06-21 17:46:27 +0900

    fix/cors (#12)
    * fix: CrossOrigin 모두 허용
    
    * fix: CrossOrigin 배포 테스트
    
    * fix: 브랜치 수정
    
    * refactor: 컨트롤러 리퀘스트 맵핑 mulit->chess로 수정
    
    * fix: 익셉션 크로스 오리진 허용
    
    * fix: 모든 도메인 허용

--- src/main/java/com/example/chessserver/chess/controller/ExceptionController.java
+++ src/main/java/com/example/chessserver/chess/controller/ExceptionController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-@CrossOrigin
+@CrossOrigin(origins = "*")
+
 public class ExceptionController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @ExceptionHandler(NoContentException.class)
--- src/main/java/com/example/chessserver/user/controller/UserController.java
+++ src/main/java/com/example/chessserver/user/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RequestMapping("users")
 public class UserController {
     private final UserService userService;
